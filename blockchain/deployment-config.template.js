6ab8f37914a04db639d8a27ca210ec710ff56cd9
Ess <127221085+EstherLavender@users.noreply.github.com>
2024-11-25 14:50:05 +0300

    Update deployment-config.template.js

--- blockchain/deployment-config.template.js
+++ blockchain/deployment-config.template.js
@@ -1,9 +1,9 @@
 const deploymentConfig = {
   // Private key of the deployer account, beginning with 0x
-  deployerPrivateKey: '0x0000000000000000000000000000000000000000000000000000000000000000',
+  deployerPrivateKey: '0x96f37ff80736d91fe5ac616539f918d568ba506ac7c1a4d5d2b02b472e3f89c8',
 
   // Full URL such as https://abc123.multibaas.com
-  deploymentEndpoint: 'https://<DEPLOYMENT ID>.multibaas.com',
+  deploymentEndpoint: 'https://rv5tmo7fsfbzrjoo4runkrfwae.multibaas.com',
 
   // The chain ID of the blockchain network
   // For example: Curvegrid test network = 2017072401, Ethereum Mainnet = 1, BSC Testnet = 97, BSC Mainnet = 56, Ethereum Sepolia = 11155111
@@ -12,7 +12,7 @@ const deploymentConfig = {
   // API key to access MultiBaas web3 endpoint
   // Note that the API key MUST be part of the "Web3" group
   // Create one on MultiBaas via navigation bar > Admin > API Keys
-  web3Key: '<API KEY IN WEB3 GROUP>',
+  web3Key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzMyNTIwNTc2LCJqdGkiOiJkZjBjZGViZC1mN2VkLTRhODQtYTg3Mi0xMDgyYTdlYmMzODYifQ.ms0t-uzvvclGcsh2l6TTHSJZ882ikHyFB25RgNtsq7o',
 
   // RPC URL of the blockchain network is required if a web3Key is not provided
   // This is required for networks that where MultiBaas does not support the web3 proxy feature
@@ -21,7 +21,7 @@ const deploymentConfig = {
   // API key to access MultiBaas from deployer
   // Note that the API key MUST be part of the "Administrators" group
   // Create one on MultiBaas via navigation bar > Admin > API Keys
-  adminApiKey: '<API KEY IN ADMINISTRATOR GROUP>',
+  adminApiKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiaWF0IjoxNzMyNTIwNjg2LCJqdGkiOiIxYjVjMTE5My1kY2Q2LTRmNzItYWExNS1mZGUzOGU5NGQ1ODAifQ.a8eXg7gJ9dv84dSKCbO9X2ANUF4H7UR_dXrruD2qvcA',
 };
 
 module.exports = {

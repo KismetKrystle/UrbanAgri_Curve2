83cc578dc3872a52808c7669a60f5b14a65e35a0
Ess <127221085+EstherLavender@users.noreply.github.com>
2024-11-26 16:29:17 +0300

    Update UrbanFarmDLP.sol

--- blockchain/contracts/UrbanFarmDLP.sol
+++ blockchain/contracts/UrbanFarmDLP.sol
@@ -1,28 +1,52 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import "@vana-dao/dlp-library.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/MulticallUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./DataLiquidityPoolStorageV1.sol";
+import "./VanaDLP.sol";
 
-contract UrbanFarmDLP is VanaDLP, UUPSUpgradeable, Ownable2StepUpgradeable, PausableUpgradeable, ReentrancyGuardUpgradeable, MulticallUpgradeable {
+contract UrbanFarmDLP is
+    VanaDLP,
+    UUPSUpgradeable,
+    PausableUpgradeable,
+    Ownable2StepUpgradeable,
+    ReentrancyGuardUpgradeable,
+    MulticallUpgradeable,
+    DataLiquidityPoolStorageV1
+{
     using SafeERC20 for IERC20;
     using ECDSA for bytes32;
     using MessageHashUtils for bytes32;
 
     // Events
-    event CommunityDataStored(uint256 indexed communityId, address indexed user);
-    event SolutionGenerated(uint256 indexed communityId, string aiSolution, address indexed user);
-    event UserInteractionLogged(uint256 indexed communityId, address indexed user, string action, string details);
-    event FeedbackSubmitted(uint256 indexed communityId, address indexed user, string feedback);
-    event CommunityRewardDistributed(uint256 indexed communityId, uint256 amount);
+    event CommunityDataStored(
+        uint256 indexed communityId,
+        address indexed user
+    );
+    event SolutionGenerated(
+        uint256 indexed communityId,
+        string aiSolution,
+        address indexed user
+    );
+    event UserInteractionLogged(
+        uint256 indexed communityId,
+        address indexed user,
+        string action,
+        string details
+    );
+    event FeedbackSubmitted(
+        uint256 indexed communityId,
+        address indexed user,
+        string feedback
+    );
+    event CommunityRewardDistributed(
+        uint256 indexed communityId,
+        uint256 amount
+    );
 
     // Structs
     struct CommunityData {
@@ -103,44 +127,70 @@ contract UrbanFarmDLP is VanaDLP, UUPSUpgradeable, Ownable2StepUpgradeable, Paus
     }
 
     // Add AI Solution
-    function addSolution(uint256 communityId, string memory aiSolution) public onlyVanaMember {
+    function addSolution(
+        uint256 communityId,
+        string memory aiSolution
+    ) public onlyVanaMember {
         require(communities[communityId].id != 0, "Community does not exist");
-        solutions[communityId].push(Solution(aiSolution, block.timestamp, msg.sender));
+        solutions[communityId].push(
+            Solution(aiSolution, block.timestamp, msg.sender)
+        );
 
         // Distribute community reward for the added solution
-        rewardToken.transfer(communities[communityId].id, communityRewardPerSolution);
-        emit CommunityRewardDistributed(communityId, communityRewardPerSolution);
+        rewardToken.transfer(
+            communities[communityId].id,
+            communityRewardPerSolution
+        );
+        emit CommunityRewardDistributed(
+            communityId,
+            communityRewardPerSolution
+        );
 
         emit SolutionGenerated(communityId, aiSolution, msg.sender);
     }
 
     // Log User Interaction
-    function logUserInteraction(uint256 communityId, string memory action, string memory details) public {
+    function logUserInteraction(
+        uint256 communityId,
+        string memory action,
+        string memory details
+    ) public {
         require(communities[communityId].id != 0, "Community does not exist");
-        userInteractions[communityId].push(UserInteraction(msg.sender, action, details, block.timestamp));
+        userInteractions[communityId].push(
+            UserInteraction(msg.sender, action, details, block.timestamp)
+        );
 
         emit UserInteractionLogged(communityId, msg.sender, action, details);
     }
 
     // Submit Feedback
-    function submitFeedback(uint256 communityId, string memory feedback) public {
+    function submitFeedback(
+        uint256 communityId,
+        string memory feedback
+    ) public {
         require(communities[communityId].id != 0, "Community does not exist");
 
         emit FeedbackSubmitted(communityId, msg.sender, feedback);
     }
 
     // Retrieve Solutions
-    function getSolutions(uint256 communityId) public view returns (Solution[] memory) {
+    function getSolutions(
+        uint256 communityId
+    ) public view returns (Solution[] memory) {
         return solutions[communityId];
     }
 
     // Retrieve User Interactions
-    function getUserInteractions(uint256 communityId) public view returns (UserInteraction[] memory) {
+    function getUserInteractions(
+        uint256 communityId
+    ) public view returns (UserInteraction[] memory) {
         return userInteractions[communityId];
     }
 
     // Upgradable contract functions
-    function _authorizeUpgrade(address newImplementation) internal virtual override onlyOwner {}
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal virtual override onlyOwner {}
 
     function version() external pure virtual override returns (uint256) {
         return 1;

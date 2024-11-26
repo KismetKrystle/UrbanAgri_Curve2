f409bf1607eea2e88fb6c94f627c0eb74da776af
Ess <127221085+EstherLavender@users.noreply.github.com>
2024-11-26 19:32:13 +0300

    Update VoteButton.tsx

--- frontend/app/components/VoteButton.tsx
+++ frontend/app/components/VoteButton.tsx
@@ -1,34 +1,34 @@
 import { FC } from "react";
 
-interface VoteButtonProps {
-  index: number;
-  voteCount: number;
+interface ActionButtonProps {
+  label: string;
+  count: number;
   isActive: boolean;
   isDisabled: boolean;
-  handleVote: (index: number) => void;
+  handleAction: () => void;
 }
 
-const VoteButton: FC<VoteButtonProps> = ({
-  index,
-  voteCount,
+const ActionButton: FC<ActionButtonProps> = ({
+  label,
+  count,
   isActive,
   isDisabled,
-  handleVote,
+  handleAction,
 }) => {
   const backgroundColor = isActive
     ? "bg-button-voted hover:bg-button-clear-vote"
-    : "bg-button-default hover:bg-button-cast-vote"
+    : "bg-button-default hover:bg-button-cast-vote";
   const cursor = isDisabled ? "cursor-not-allowed" : "cursor-pointer";
 
   return (
     <button
-      className={`vote-button ${backgroundColor} ${cursor}`}
-      onClick={() => handleVote(index)}
+      className={`action-button ${backgroundColor} ${cursor}`}
+      onClick={handleAction}
       disabled={isDisabled}
     >
-      Option {index + 1}: {voteCount} votes
+      {label}: {count}
     </button>
   );
 };
 
-export default VoteButton;
+export default ActionButton;
